31ae81cd11567f35f841f4078cb95ab7327928b9
Christof Stocker <c.stocker@hci-kdd.org>
2016-05-14 18:26:42 +0200

    add more tests for pipeline

--- test/tst_pipeline.jl
+++ test/tst_pipeline.jl
@@ -107,5 +107,12 @@ end
     @testset "concrete pipeline and image" begin
         pl = LinearPipeline([FlipX(), FlipY(), CropRatio(1.5), Resize(64,64)])
         @imagetest "LinearPipeline" transform(pl, testimg)
+
+        testimgs = Array{Image,1}()
+        push!(testimgs, testimg)
+        push!(testimgs, testimg)
+        imgs = transform(pl, testimgs)
+        @imagetest "LinearPipeline" imgs[1]
+        @imagetest "LinearPipeline" imgs[2]
     end
 end
